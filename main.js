4b8ddcc092ac9f84bfcd46fdd9987838b32da09f
AadityaKajla <101193172+AadityaKajla@users.noreply.github.com>
2022-10-21 20:13:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+}
+function draw(){
+    image(video,0,0,300,300);
+}
+function takeSnapshot(){
+    save('myimage.png');
+}
